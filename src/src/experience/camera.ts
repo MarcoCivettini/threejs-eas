3ae88ebc5966e634c3f1c01e90909b5f616840fe
MarcoCivettini <civettini.marco97@gmail.com>
2023-04-03 21:25:40 +0200

    [Feat] moved files from .js to .ts

---
+++ src/src/experience/camera.ts
@@ -0,0 +1,48 @@
+import Experience from "./experience";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'three';
+
+
+export default class Camera {
+    experience: Experience;
+    sizes: any;
+    scene: any;
+    canvas: any;
+    instance: THREE.PerspectiveCamera;
+    controls?: OrbitControls;
+
+    constructor() {
+        this.experience = new Experience()
+        this.sizes = this.experience.sizes
+        this.scene = this.experience.scene
+        this.canvas = this.experience.canvas
+
+        // set camera instance
+        this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 100)
+        this.instance.position.set(6, 4, 8)
+        this.scene.add(this.instance)
+        // this.setInstance();
+        // this.setOrbitControls();
+    }
+
+    // setInstance() {
+    //     this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 100)
+    //     this.instance.position.set(6, 4, 8)
+    //     this.scene.add(this.instance)
+    // }
+
+    setOrbitControls() {
+        this.controls = new OrbitControls(this.instance, this.canvas)
+        this.controls.enableDamping = true
+    }
+
+    resize() {
+        this.instance.aspect = this.sizes.width / this.sizes.height
+        this.instance.updateProjectionMatrix()
+    }
+
+    update(){
+        // this.controls.update()
+    }
+
+}
