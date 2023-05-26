2517d20095aacb3f0b14fd558bc58b5ffc6fa522
MarcoCivettini <civettini.marco97@gmail.com>
2023-05-27 00:54:20 +0200

    [Feat] Added smooth rotation on move

--- src/src/experience/world/character-controller.ts
+++ src/src/experience/world/character-controller.ts
@@ -1,6 +1,6 @@
 import { MathUtils, Quaternion, Vector3 } from "three";
 import { Component } from "../models/component";
-import { getAngleDegrees, getAngleDegreesFromAnotherAngle } from "../utils/angle";
+import { getAngleDegreesFromAnotherAngle } from "../utils/angle";
 import * as CANNON from 'cannon-es';
 import { InputState } from "../models/input-state";
 
@@ -8,16 +8,15 @@ import { InputState } from "../models/input-state";
 export class ChartacterController extends Component {
     private inputState?: InputState;
     private speed: number;
-    currentAngleRad: number = 0;
-    currentPosition = new Vector3();
-    targetPosition = new Vector3();
-    targetVelocity = new CANNON.Vec3();
-    currentVelocity = new CANNON.Vec3();
+    private targetVelocity = new CANNON.Vec3();
+    private currentVelocity = new CANNON.Vec3();
+    private targetPosition = new Vector3();
+    private targetQuaternion = new Quaternion();
+    private currentQuaternion = new Quaternion();
+    private readonly rotationSpeed = 0.08;
     constructor(speed: number) {
         super('characterController')
         this.speed = speed;
-
-
     }
 
     protected onRegistration(): void {
@@ -25,6 +24,9 @@ export class ChartacterController extends Component {
 
         this.targetPosition.copy(parent.physicsBody.position)
             .add(new Vector3(0, 0, 0));
+
+        this.currentQuaternion.copy(parent.physicsBody.quaternion);
+        this.targetQuaternion.copy(parent.physicsBody.quaternion);
     }
 
     withInputController(inputController: InputState): this {
@@ -51,61 +53,8 @@ export class ChartacterController extends Component {
             if (this.inputState.down) zSpeed += this.speed; // Down arrow
 
             if (xSpeed || zSpeed) {
-                this.targetVelocity.set(xSpeed, parent.physicsBody.velocity.y, zSpeed);
-
-
-                // const velocityDiff = this.targetVelocity.vsub(this.currentVelocity);
-
-                // Define the interpolation factor (adjust as needed)
-                const interpolationFactor = 0.08;
-
-                // Interpolate the current velocity towards the target
-                this.currentVelocity.lerp(this.targetVelocity, interpolationFactor, this.currentVelocity);
-
-
-                // Update the physics body's velocity
-                parent.physicsBody.velocity.copy(this.currentVelocity);
-                parent.physicsBody.applyImpulse(this.currentVelocity);
-
-
-                // if (parent.physicsBody.velocity.z === 0 && parent.physicsBody.velocity.x === 0) {
-                //     parent.physicsBody.applyImpulse(new CANNON.Vec3(xSpeed, 0, zSpeed));
-                // } else {
-                //     parent.physicsBody.velocity.x = xSpeed;
-                //     parent.physicsBody.velocity.z = zSpeed;
-                // }
-
-
-                // const targetAngleDeg = this.getAngleDegrees(xSpeed, -zSpeed);
-                const targetAngleDeg = getAngleDegreesFromAnotherAngle(0, 1, xSpeed, -zSpeed);
-                const targetAngleRad = MathUtils.degToRad(targetAngleDeg);
-
-
-                // if (!this.currentAngleRad) {
-                //     this.currentAngleRad = parent.physicsBody.quaternion.y;
-
-                // }
-
-                // Calculate the rotation difference between the current and target angles
-                let angleDiff = targetAngleRad - this.currentAngleRad;
-
-                // Normalize the angle difference to be between -π and π
-                angleDiff = ((angleDiff + Math.PI) % (2 * Math.PI)) - Math.PI;
-                console.log(angleDiff);
-                // Define the rotation speed (adjust as needed)
-                const rotationSpeed = 0.08;
-
-                // Gradually rotate towards the target angle
-                if (Math.abs(angleDiff) > rotationSpeed) {
-                    const rotationDirection = angleDiff > 0 ? 1 : -1;
-                    console.log(rotationSpeed * rotationDirection)
-                    this.currentAngleRad += rotationSpeed * rotationDirection;
-                } else {
-                    this.currentAngleRad = targetAngleRad;
-                }
-
-                var qy = new Quaternion().setFromAxisAngle(new Vector3(0, 1, 0), this.currentAngleRad);
-                parent.physicsBody.quaternion.copy(qy as any);
+                this.setVelocity(parent, xSpeed, zSpeed);
+                this.SmoothRotation(parent, xSpeed, zSpeed)
             }
 
 
@@ -113,6 +62,24 @@ export class ChartacterController extends Component {
             parent.physicsBody.velocity.x = 0;
             parent.physicsBody.velocity.z = 0;
         }
+    }
+
+    private setVelocity(parent: any, xSpeed: number, zSpeed: number): void {
+        this.targetVelocity.set(xSpeed, parent.physicsBody.velocity.y, zSpeed);
+
+        // Interpolate the current velocity towards the target
+        this.currentVelocity.lerp(this.targetVelocity, this.rotationSpeed, this.currentVelocity);
+        // Update the physics body's velocity
+        parent.physicsBody.velocity.copy(this.currentVelocity);
+        parent.physicsBody.applyImpulse(this.currentVelocity);
+    }
+
+    private SmoothRotation(parent: any, xSpeed: number, zSpeed: number): void {
+        const targetAngleDeg = getAngleDegreesFromAnotherAngle(0, 1, xSpeed, -zSpeed);
+        const targetAngleRad = MathUtils.degToRad(targetAngleDeg);
+        this.targetQuaternion.setFromAxisAngle(new Vector3(0, 1, 0), targetAngleRad);
+        this.currentQuaternion = new Quaternion().slerpQuaternions(this.currentQuaternion, this.targetQuaternion, this.rotationSpeed);
+        parent.physicsBody.quaternion.copy(this.currentQuaternion);
     }
 }
 
